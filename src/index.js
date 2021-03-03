ee359c55e9d2c974c35fe3a6b569a2b6ac293789
Brandon <estanob@gmail.com>
2021-03-03 10:49:39 -0800

    Add more popups

--- src/index.js
+++ src/index.js
@@ -7,5 +7,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   minZoom: 6,
 }).addTo(mymap);
 
-var commHillMarker = L.marker([37.29464, -121.86944]).addTo(mymap); //459 Mill Pond Drive, San Jose, CA
-var bevHillsMarker = L.marker([34.08536, -118.40769]).addTo(mymap); //901 North Alpine Drive, Beverly Hills, CA
+var commHillMarker = L.marker([37.29464, -121.86944]).addTo(mymap); //459 Mill Pond Drive, San Jose
+var bevHillsMarker = L.marker([34.08536, -118.40769]).addTo(mymap); //901 North Alpine Drive, Beverly Hills
+var sanDiegoMarker = L.marker([32.75702, -117.14421]).addTo(mymap); //1929 Meade Avenue, San Diego
+
+commHillMarker.bindPopup("<b>Move here!</b><br>Communications Hill, <br> San Jose, CA");
+bevHillsMarker.bindPopup("<b>Move here!</b><br>Beverly Hills, CA");
+sanDiegoMarker.bindPopup("<b>Move here!</b><br>San Diego, CA");
