72136ae8f7e2d15c99963df9fa0eb34cce784b72
Brandon <estanob@gmail.com>
2021-03-02 21:56:56 -0800

    Add markers to map for houses

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([37.405, -119.312], 6);
+// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
-  maxZoom: 18,
-  // minZoom: 10,
-  // id: 'mapbox/streets-v11',
-  // tileSize: 512,
-  // zoomOffset: -1,
-  // accessToken: 'your.mapbox.access.token'
+  maxZoom: 19,
+  minZoom: 6,
 }).addTo(mymap);
+
+var commHillMarker = L.marker([37.29464, -121.86944]).addTo(mymap); //459 Mill Pond Drive, San Jose, CA
+var bevHillsMarker = L.marker([34.08536, -118.40769]).addTo(mymap); //901 North Alpine Drive, Beverly Hills, CA
